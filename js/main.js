b99d1a16e76f958c43056d270bdfff3c69043b88
unknown <bakaric.m@gmail.com>
2019-02-13 16:31:15 +0100

    jasmin testiranje funkcije

--- js/main.js
+++ js/main.js
@@ -1,12 +1,1 @@
-function sum(x, y) {
-    // proveriti da li je jedan od argumenata string
-    if (typeof x === 'number' && typeof y === 'number')
-        return +(x + y).toFixed(10);
-        return NaN;  // ako je jedan od argumenata string vratiti NaN
-    
-  
-   
-   
-}
-console.log(sum(0.00002,0.00004));
 
